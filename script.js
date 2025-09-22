ca19fed4bb34e4b235a7bfc0090820a05fdb0c42
podu666 <egorshkarupa123456@mail.ru>
2025-09-22 23:10:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+// Задание 1
+/* a = "Teкст из HTML"
+b = "Teкст из JS"
+console.log(b)
+console.log(a)*/
+
+// Задание 2
+
+
+/*num = 123
+a = 5
+a = 7
+b = 1+2+3
+
+console.log(num)
+console.log(a)
+console.log(b)*/
+
+// Задание 3
+
+/*c = 10
+d = 2
+
+a = c+d
+b = c-d
+v = c*d
+z = c/d
+
+console.log(a,b,v,z)*/
+
+//Задание 4
+
+/*
+  1.let a = 5 + 5 * 3 + 3;
+  Результат: 23
+
+  2. let a = 8 / 2 + 2;
+  Результат: 6
+  
+  3. let a = (2 + 3) * (2 + 3);
+//   Результат: 25
+
+//   4. let a = (2 + 3) * 2 + 3;
+//   Результат: 13
+
+//   5. let a = (2 * 8) / 4;
+//   Результат: 4
+
+//   6. let a = 2 * (8 / 4);
+//   •  Результат: 4
+//   */
+
+//   // Задание 5
+
+  
+//   /*a = 1.5
+//   b = 0.75
+//   c = a+b
+
+//   f = -5
+
+//   console.log(c)
+//   console.log(f*(-1))*/
+  
+//   //Задание 6
+
+//   /*a = "Шкарупа"
+//   b = "Егор"
+// console.log(a,b)
+
+//  c = "Java"
+//   d = "Script"
+// console.log(c+d)
+
+//  e = "Hello"
+//   f = "World"
+// console.log(e,f)*/
+
+// // function Hello() {
+// //     console.log("Hello func");
+// // }
+// // Hello()
+
+// // const salary = (money) => {
+// //   money = money * 0.87
+// //   return money * 0.75
+// // }
+// // const userMoney =  Number(prompt("Сколько ты зарабатываешь"))
+// // let moneyMonth = salary(userMoney)
+
+// // console.log(`Для расходов можно использовать ${salary(userMoney)}`);
+// // console.log(`На еду желательно тратить  ${moneyMonth * 0.3}`);
